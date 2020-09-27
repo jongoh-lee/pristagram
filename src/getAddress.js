a2afee9e94ce5b4e4a3f203d1685c6bfce819d75
jongoh lee <jongohlee@jongohui-MacBookPro.local>
2020-09-27 18:31:36 +0900

    hide google key

--- src/getAddress.js
+++ src/getAddress.js
@@ -1,7 +1,8 @@
 const fetch = require("node-fetch");
 
+const googleKey = process.env.GOOGLE_API_KEY;
 let date;
-fetch("https://maps.googleapis.com/maps/api/geocode/json?sensor=false&language=ko&address="+ encodeURIComponent("서울시 용산구 이태원동") + "&key=AIzaSyB77l5gKZERH5nDSvT5PboXLkl_uJbVcUQ").then(e => e.json()).then(e => e == date);
+fetch("https://maps.googleapis.com/maps/api/geocode/json?sensor=false&language=ko&address="+ encodeURIComponent("서울시 용산구 이태원동") + `&key=${googleKey}`).then(e => e.json()).then(e => e == date);
 
 console.log(date);
 
