c3cd0a1e3f27b9047c79f6004259f287143ed4a4
jongoh lee <jongohlee@jongohui-MacBookPro.local>
2020-10-19 22:45:16 +0900

    loadMoreUserPost

--- src/api/Post/loadMorePost/loadMorePost.js
+++ src/api/Post/loadMorePost/loadMorePost.js
@@ -2,11 +2,9 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    loadMorePost:async (_, args, { request, isAuthenticated }) => {
-      isAuthenticated(request);
-      const { id } = args;
-      const { user } = request;
-      return await prisma.user({id: user.id}).posts({orderBy:"createdAt_DESC", first:9, after: id});
+    loadMorePost:async (_, args) => {
+      const { id, username } = args;
+      return await prisma.user({username: username}).posts({orderBy:"createdAt_DESC", first:9, after: id});
     }
   }
 };
